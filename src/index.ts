5b60db8a9f173b44ce1b4120643b6e4a41c1e5fc
EdwinAlmendras <xxxedwin10xxx@gmail.com>
2021-03-13 18:36:56 +0100

    new

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 
-
-console.log("working in typescript")
-
-
-import User from "core/user";
 import { login } from "./auth";
-
-let email = "eafeik7@gmail.com"
-let password = "zxcvbnm"
+export default login 
 
 
-async function main() {
+/* async function main() {
     const user: User = await login({
         email, password
     }, null)
@@ -19,15 +12,4 @@ async function main() {
     const files = await user.getFiles()
     await files.fetch()
     await files.shortcut({ name: "csac"}, { props: { name: "sacaa"}})
-//await files.update({ name: "csac", properties: { uid: "saddas"}})
-
-//console.log(await files.get({ name: "csac"}))
-/*     console.log(await files.get({ name: "sacaa"}))
-
- */
-   /* const url = await files.export({name: "dsadsad"}) */
-
-/* await files.import({name: "tt"},url) */
-
-
-main()
+main() */
